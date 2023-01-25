d4710eafcaa80ffe61add58a4afb9a27478ecc09
dh12091209 <83049395+dh12091209@users.noreply.github.com>
2023-01-24 21:26:41 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,17 @@ public class Main {
         tree.add(34);
         tree.add(120);
         tree.add(222);
+        tree.add(74);
+        tree.add(34);
+        tree.add(38);
+        tree.add(40);
+        tree.add(37);
+        tree.add(22);
+
 
         tree.printInOrder();
         System.out.println(tree.exists(1));
+        tree.remove(50);
+        tree.printInOrder();
     }
 }
